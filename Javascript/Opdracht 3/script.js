dd0d64ee165f4ce532ae3f43ffa102b7ce02997a
MarliesP <marliespetter@hotmail.com>
2021-09-17 12:20:35 +0200

    Opdr3.

---
+++ Javascript/Opdracht 3/script.js
@@ -0,0 +1,23 @@
+const age = 26;
+
+if (age >= 18) {
+    console.log("Je bent welkom");
+} else {
+    console.log("Je bent te jong!")
+};
+
+const isFemale = true;
+
+if (isFemale == true) {
+    console.log("You're welcome")
+} else {
+    console.log("No party for you tonight")
+};
+
+const driverStatus = 'Henk';
+
+if (driverStatus == 'Bob') {
+    console.log('You are Bob and are allowed to take your friends save home, good job!')
+} else {
+    console.log('You are not Bob and are not allowed to drive. Seek your Bob, to drive you home.')
+};
