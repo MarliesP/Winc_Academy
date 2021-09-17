9ebb9923b1f491afc66fef3f5278bec009054a9b
MarliesP <marliespetter@hotmail.com>
2021-09-17 11:46:13 +0200

    JS opdr2

---
+++ Javascript/Opdracht 2/script.js
@@ -0,0 +1,7 @@
+console.log("Hallo Winc Academy studenten")
+//dit is een rekensommetje
+/*
+let uitkomst1 = 1230941 * 1823794;
+console.log(uitkomst1);
+let uitkomst2 = 637263 / 54;
+console.log(uitkomst1, uitkomst2);*/
