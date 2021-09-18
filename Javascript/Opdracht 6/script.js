7e8895ad7e38d04fd762af80416011c0fce033d7
MarliesP <marliespetter@hotmail.com>
2021-09-18 20:50:49 +0200

    Opdr 6

---
+++ Javascript/Opdracht 6/script.js
@@ -0,0 +1,19 @@
+function pizzaMaker() {
+    console.log("Stap 1, koop een pizza");
+    console.log("Stap 2, verwarm de oven");
+    console.log("stap 3, doe de pizza in de oven");
+    console.log("Stap 4, haal pizza eruit en enjoy");
+};
+
+function sushiMaker() {
+    console.log("Stap 1, Koop suhi spullen");
+    console.log("Stap 2, Snijden maar");
+    console.log("stap 3, Rollen wat je rollen kunt");
+    console.log("Stap 4, Eten wat je tussen je stokjes kunt krijgen");
+};
+
+sushiMaker();
+sushiMaker();
+sushiMaker();
+pizzaMaker();
+pizzaMaker();
